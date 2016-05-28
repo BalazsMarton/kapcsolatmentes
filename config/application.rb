0f9759d654cb56c92a02cba782a8824206167527
BalazsMarton <balazsmarton90.joomla@gmail.com>
2016-05-28 12:22:52 +0200

    style change from owner mail

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,8 @@ Bundler.require(*Rails.groups)
 module Kapcsolatmentes
   class Application < Rails::Application
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
-config.assets.precompile += Ckeditor.assets
-config.assets.precompile += %w(ckeditor/*)
+    config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w(ckeditor/*)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
