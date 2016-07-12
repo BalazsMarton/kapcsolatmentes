afbaaa23e76b13661d584fd290949048831955cb
BalazsMarton <balazsmarton90.joomla@gmail.com>
2016-07-13 00:46:54 +0200

    image resize to fill

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -42,7 +42,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   version :contentimg do
-    process :resize_to_fit => [540, 300]
+    process :resize_to_fill => [716, 300]
   end
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
